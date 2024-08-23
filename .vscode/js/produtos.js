920f83281b334b7a0452da4122635482857a4397
gigigisa <cherryblossomcb24@gmail.com>
2024-08-23 20:35:14 -0300

    criaçao das modal da pagina de produtos gisa julio amanda geovana isis

---
+++ .vscode/js/produtos.js
@@ -0,0 +1,51 @@
+
+
+const modal = document.querySelector("#modalNovo") //modal
+const botao = document.querySelector("#btn50") //botao
+
+botao.addEventListener("click",function(){
+    modal.style.display = 'block' // visible = torna a div visível --> hidden = torna invisível
+})
+
+const modal2 = document.querySelector("#modal2") //modal
+const botao2 = document.querySelector("#btn30") //botao
+
+botao2.addEventListener("click",function(){
+    modal2.style.display = 'block' // visible = torna a div visível --> hidden = torna invisível
+})
+
+const modal3 = document.querySelector("#modal3") //modal
+const botao3 = document.querySelector("#btn40") //botao
+
+botao3.addEventListener("click",function(){
+    modal3.style.visibility = 'visible' // visible = torna a div visível --> hidden = torna invisível
+})
+
+const modal4 = document.querySelector("#modal4") //modal
+const botao4 = document.querySelector("#btn20") //botao
+
+botao4.addEventListener("click",function(){
+    modal4.style.visibility = 'visible' // visible = torna a div visível --> hidden = torna invisível
+})
+
+const modal5 = document.querySelector("#modal5") //modal
+const botao5 = document.querySelector("#btn70") //botao
+
+botao5.addEventListener("click",function(){
+    modal5.style.visibility = 'visible' // visible = torna a div visível --> hidden = torna invisível
+})
+
+const modal6 = document.querySelector("#modal6") //modal
+const botao6 = document.querySelector("#btn10") //botao
+
+botao6.addEventListener("click",function(){
+    modal6.style.visibility = 'visible' // visible = torna a div visível --> hidden = torna invisível
+})
+
+
+
+
+
+
+
+
