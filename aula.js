d1e8c9e7b68e43a425e060a48f3dc413f7a7d227
gigigisa <cherryblossomcb24@gmail.com>
2024-09-06 20:26:59 -0300

    gege e isis, arrumamos tudo que conseguimos nas paginas inicial,cadastro e produtos ;)

---
+++ aula.js
@@ -0,0 +1,11 @@
+let senhaCorreta = "1112";
+let senha = prompt("digite uma senha:");
+
+while (senha !== senhaCorreta) {
+alert("senha incorreta. Digite de novo, mula");
+senha = prompt("digite sua senha:");
+
+
+}
+
+alert("senha correta!");
