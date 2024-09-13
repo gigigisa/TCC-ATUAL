3fc8f72254fa8d57de1db6f4a3f8e670144c04d5
gigigisa <cherryblossomcb24@gmail.com>
2024-09-13 19:30:27 -0300

    modais funcionando

--- js/produtos.js
+++ js/produtos.js
@@ -1,6 +1,30 @@
 
+//MODAL DE 50
 
-//modal 50
+const modal50 = document.querySelector("#modal50")
+const botaoabrir50 = document.querySelector ("#btn50")
+const botaofechar50 = document.querySelector ("#btnfechar50")
+
+botaoabrir50.addEventListener("click",function(){
+   modal50.showModal()
+})
+
+botaofechar50.addEventListener("click",function(){
+   modal50.close()
+})
+
+//MODAL DE 30%
+const modal30 = document.querySelector("#modal30")
+const botaoabrir30 = document.querySelector ("#btn30")
+const botaofechar30 = document.querySelector ("#btnfechar30")
+
+botaoabrir30.addEventListener("click",function(){
+   modal30.showModal()
+})
+
+botaofechar30.addEventListener("click",function(){
+   modal30.close()
+})
 
 
 
@@ -12,5 +36,11 @@
 
 
 
-//modal 40
+
+
+
+
+
+
+
 
