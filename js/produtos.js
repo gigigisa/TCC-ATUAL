acd1e83d4067cb4554387423a4f77a26e3677635
gigigisa <cherryblossomcb24@gmail.com>
2024-09-20 19:16:11 -0300

    Gisa, julio fazendo modal de suporte na pagina inicial

--- js/produtos.js
+++ js/produtos.js
@@ -25,8 +25,56 @@ botaoabrir30.addEventListener("click",function(){
 botaofechar30.addEventListener("click",function(){
    modal30.close()
 })
+//modal40
 
+const modal40 = document.querySelector("#modal40")
+const botaoabrir40 = document.querySelector ("#btn40")
+const botaofechar40 = document.querySelector ("#btnfechar40")
 
+botaoabrir40.addEventListener("click",function(){
+   modal40.showModal()
+})
+
+botaofechar40.addEventListener("click",function(){
+   modal30.close()
+})
+
+//modaol20
+const modal20 = document.querySelector("#modal20")
+const botaoabrir20 = document.querySelector ("#btn20")
+const botaofechar20 = document.querySelector ("#btnfechar20")
+
+botaoabrir20.addEventListener("click",function(){
+   modal20.showModal()
+})
+
+botaofechar20.addEventListener("click",function(){
+   modal30.close()
+})
+//modal70
+const modal70 = document.querySelector("#modal70")
+const botaoabrir70 = document.querySelector ("#btn70")
+const botaofechar70 = document.querySelector ("#btnfechar70")
+
+botaoabrir70.addEventListener("click",function(){
+   modal70.showModal()
+})
+
+botaofechar70.addEventListener("click",function(){
+   modal70.close()
+})
+//modal10
+const modal10 = document.querySelector("#modal10")
+const botaoabrir10 = document.querySelector ("#btn10")
+const botaofechar10 = document.querySelector ("#btnfechar10")
+
+botaoabrir10.addEventListener("click",function(){
+   modal10.showModal()
+})
+
+botaofechar10.addEventListener("click",function(){
+   modal10.close()
+})
 
 
 
