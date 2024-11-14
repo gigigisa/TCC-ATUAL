2a7604cd99e1eebd406593dc25c133e3b7591daa
gigigisa <cherryblossomcb24@gmail.com>
2024-11-14 17:57:01 -0300

    gege e amandinha arrumamos os codigos

--- js/produtos.js
+++ js/produtos.js
@@ -1,6 +1,4 @@
 
-//MODAL DE 50
-
 const modal50 = document.querySelector("#modal50")
 const botaoabrir50 = document.querySelector ("#btn50")
 const botaofechar50 = document.querySelector ("#btnfechar50")
@@ -13,7 +11,7 @@ botaofechar50.addEventListener("click",function(){
    modal50.close()
 })
 
-//MODAL DE 30%
+
 const modal30 = document.querySelector("#modal30")
 const botaoabrir30 = document.querySelector ("#btn30")
 const botaofechar30 = document.querySelector ("#btnfechar30")
@@ -25,7 +23,6 @@ botaoabrir30.addEventListener("click",function(){
 botaofechar30.addEventListener("click",function(){
    modal30.close()
 })
-//modal40
 
 const modal40 = document.querySelector("#modal40")
 const botaoabrir40 = document.querySelector ("#btn40")
@@ -39,7 +36,7 @@ botaofechar40.addEventListener("click",function(){
    modal40.close()
 })
 
-//modaol20
+
 const modal20 = document.querySelector("#modal20")
 const botaoabrir20 = document.querySelector ("#btn20")
 const botaofechar20 = document.querySelector ("#btnfechar20")
@@ -51,7 +48,7 @@ botaoabrir20.addEventListener("click",function(){
 botaofechar20.addEventListener("click",function(){
    modal20.close()
 })
-//modal70
+
 const modal70 = document.querySelector("#modal70")
 const botaoabrir70 = document.querySelector ("#btn70")
 const botaofechar70 = document.querySelector ("#btnfechar70")
@@ -63,7 +60,8 @@ botaoabrir70.addEventListener("click",function(){
 botaofechar70.addEventListener("click",function(){
    modal70.close()
 })
-//modal10
+
+
 const modal10 = document.querySelector("#modal10")
 const botaoabrir10 = document.querySelector ("#btn10")
 const botaofechar10 = document.querySelector ("#btnfechar10")
